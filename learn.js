d944ef0712ea9979d235f85d9e49f018c7ea5a1a
EL-JO-DEV <youssef714@gmail.com>
2024-11-27 16:38:37 +0200

    changed

--- learn.js
+++ learn.js
@@ -1,5 +1,16 @@
 // i made this file to write all commands for git hub 
 
 /*
-  1- clone : Clone a repository into a new directory 
+  1-  clone : Clone a repository into a new directory 
+  2-  add   :  Add files 
+  3- commit :   Record changes to the repository
+  4-  reset :    Reset current HEAD to the specified state
+  5-   push :      Update remote refs along with associated objects
+  6- branch : main
+  7- remote : origin
+
+  how to add file to the remote repo 
+  1- git add
+  2- git commit -m "lol"
+  3- git push origin main
 */
