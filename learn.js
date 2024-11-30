b5924504cbd79ee27dc945152dd0751ccb2e635a
EL-JO-DEV <youssef714@gmail.com>
2024-11-30 15:22:04 +0200

    lol

--- learn.js
+++ learn.js
@@ -1,16 +1,26 @@
-// i made this file to write all commands for git hub 
+// i made this file to write all commands for github 
 
 /*
   1-  clone : Clone a repository into a new directory 
-  2-  add   :  Add files 
-  3- commit :   Record changes to the repository
-  4-  reset :    Reset current HEAD to the specified state
-  5-   push :      Update remote refs along with associated objects
+  2-  add   : Add files 
+  3- commit : Record changes to the repository
+  4-  reset : Reset current HEAD to the specified state
+  5-   push : Update remote refs along with associated objects
   6- branch : main
   7- remote : origin
 
   how to add file to the remote repo 
-  1- git add
-  2- git commit -m "lol"
-  3- git push origin main
+    1- git add
+    2- git commit -m "lol"
+    3- git push origin main
+  
+  or create a new repository on the command line
+    git init
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin git@github.com:EL-JO-DEV/l.git
+    git push -u origin main
+  
+  to have a shortcut  
+    git config --global alias.st status
 */
